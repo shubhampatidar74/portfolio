20195edc7f49defc169af56515ad7e5c60c374ab
Shubham patidar <141166263+shubhampatidar74@users.noreply.github.com>
2024-03-22 07:57:00 +0530

    my portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleButton = document.querySelector('#menuopen');
+const navbarLinks = document.querySelector('.navbar-links');
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+  toggleButton.classList.toggle('ri-close-large-line')
+});
+
